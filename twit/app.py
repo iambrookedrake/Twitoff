c1e9f10f7b52e04066762614a1fdaf9e496e6381
Brooke Drake <iambrookedrake@gmail.com>
2020-10-06 21:10:16 -0600

    assn3.3.3

--- twit/app.py
+++ twit/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from .db_model import DB, User
 from .twitter import add_user_tweepy
+from .predict import predict_user
 
 def create_app():
     '''Create and configure an instance of our Flask application'''
@@ -29,5 +30,27 @@ def create_app():
             tweets = []
             
         return render_template('user.html', title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        tweet_text = request.values['tweet_text']
+
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves! Select a second User.'
+        else:
+            prediction = int(predict_user(user1, user2, tweet_text)*100)
+            if prediction >= 50:
+                message = f'"{tweet_text}" is more likely to be said by {user1} than {user2}, with {prediction}% confidence.'
+            else:
+                message = f'"{tweet_text}" is more likely to be said by {user2} than {user1}, with {100-prediction}% confidence.'
+        
+        return render_template('predict.html', title='Prediction', message=message)
+
+    @app.route('/reset304')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
 
     return app
