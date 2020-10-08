8992c9c475a187bda718233c577c63e8dd78f05e
Brooke Drake <iambrookedrake@gmail.com>
2020-10-07 20:53:01 -0600

    .

--- twit/app.py
+++ twit/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from .db_model import DB, User
-from .twitter import add_user_tweepy
+from .twitter import add_user_tweepy, update_all_users
 from .predict import predict_user
 
 def create_app():
@@ -52,5 +52,10 @@ def create_app():
     def reset():
         DB.drop_all()
         DB.create_all()
+
+    @app.route('/update', methods=['GET'])
+    def update():
+        update_all_users()
+        return render_template('base.html', title='All Tweets Updated!', users=User.query.all())
 
     return app
